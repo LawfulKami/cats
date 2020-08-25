6bc3a8a75040239ecbb3d700a9b17b2ccbd850e3
Lanny Rivest <lanny.rivest@gmail.com>
2020-08-25 19:43:36 -0400

    Testing

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -2,13 +2,12 @@ const fs = require('fs');
 
 const breedDetailsFromFile = function(breed, callback) {
 
-  console.log('breedDetailsFromFile: Calling readFile...');
   
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
 
-    console.log("In readFile's Callback: it has the data.");
 
     if (!error) callback(data);
+    if (error) callback(undefined);
   });
 };
 
@@ -18,6 +17,4 @@ const callback = (data) => {
 
 
 
-// we try to get the return value
-breedDetailsFromFile('Bombay', callback);
-// console.log('Return Value: ', bombay); // => will NOT print out details, instead we will see undefined!
+module.exports = breedDetailsFromFile;
---
+++ test/asyncBreedsTest.js
@@ -0,0 +1,28 @@
+const assert = require('chai').assert;
+const breedDetailsFromFile = require('../asyncBreeds');
+
+const callback = (data) => {
+  console.log(data);
+};
+const expectedDescBa = "Balinese are curious, outgoing, intelligent cats with excellent communication skills. They are known for their chatty personalities and are always eager to tell you their views on life, love, and what you’ve served them for dinner.";
+const expectedDescBo = "The golden eyes and the shiny black coat of the Bombay is absolutely striking. Likely to bond most with one family member, the Bombay will follow you from room to room and will almost always have something to say about what you are doing, loving attention and to be carried around, often on his caregiver's shoulder.";
+const bombay = breedDetailsFromFile('Bombay', callback);
+const balinese = breedDetailsFromFile("Balinese", callback);
+
+describe('#breedDetailsFromFile', (done) => {
+
+  it('provides, via callback, breed details for the Bombay breed', (done) => {
+    breedDetailsFromFile('Bombay', (bombay) => {
+      assert.equal(expectedDescBo, bombay);
+      done();
+    });
+  });
+
+  it('provides, via callback, undefined for a breed that does not exist', (done) => {
+    breedDetailsFromFile('Saphire', (desc) => {
+      assert.equal(undefined, desc);
+      done();
+    });
+  });
+
+});
